34be541db7c5d0d554de4666804b8a45f74f4536
Sherry Yuan <frostyyshadows@gmail.com>
2018-09-15 01:23:16 -0400

    added basic recyclerview

---
+++ app/src/main/java/com/squad/betakua/adherence_app/AdherenceAdapter.java
@@ -0,0 +1,59 @@
+package com.squad.betakua.adherence_app;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by sherryuan on 2018-09-14.
+ */
+
+public class AdherenceAdapter extends RecyclerView.Adapter<AdherenceAdapter.AdherenceViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class AdherenceViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public AdherenceViewHolder(CardView v) {
+            super(v);
+            mTextView = v.findViewById(R.id.info_text);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public AdherenceAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public AdherenceAdapter.AdherenceViewHolder onCreateViewHolder(ViewGroup parent,
+                                                            int viewType) {
+        // create a new view
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_adherence_list_card, parent, false);
+        AdherenceViewHolder vh = new AdherenceViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(AdherenceViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/squad/betakua/adherence_app/MainActivity.java
+++ app/src/main/java/com/squad/betakua/adherence_app/MainActivity.java
@@ -1,13 +1,35 @@
 package com.squad.betakua.adherence_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    String[] myDataset = new String[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        myDataset[0] = "hello";
+        myDataset[1] = "world";
+        mAdapter = new AdherenceAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
