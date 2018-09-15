90a1d71f987b658ec93c5d80e3d7bb883bd33dd5
Russell Blickhan <rwblickhan@gmail.com>
2018-09-15 16:18:28 -0400

    Refactor adapter to use data source directly

--- app/src/main/java/com/squad/betakuma/adherence_app/SwipableCards/CardPagerAdapter.java
+++ app/src/main/java/com/squad/betakuma/adherence_app/SwipableCards/CardPagerAdapter.java
@@ -12,29 +12,51 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squad.betakuma.adherence_app.R;
+import com.squad.betakuma.adherence_app.data_model.DataListener;
+import com.squad.betakuma.adherence_app.data_model.MedicationManager;
+import com.squad.betakuma.adherence_app.data_model.Prescription;
+import com.squad.betakuma.adherence_app.utilities.Installation;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CardPagerAdapter extends PagerAdapter implements CardAdapter {
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
+public class CardPagerAdapter extends PagerAdapter implements CardAdapter, DataListener {
 
     private Context mContext;
     private ViewPager mParentViewPager;
     private List<CardView> mViews;
     private List<CardItem> mData;
     private float mBaseElevation;
+    private MedicationManager manager;
 
     public CardPagerAdapter(Context context, ViewPager viewPager) {
         mContext = context;
         mParentViewPager = viewPager;
         mData = new ArrayList<>();
         mViews = new ArrayList<>();
+        manager = MedicationManager.getInstance(Installation.id(context));
+        manager.registerListener(this);
+//        for (Prescription prescription : manager.getDataset()) {
+//            addPrescriptionCardItem(prescription);
+//        }
+        addCardItem(new CardItem(R.string.title_1, R.string.text_1));
+        addCardItem(new CardItem(R.string.title_2, R.string.text_1));
+        addCardItem(new CardItem(R.string.title_3, R.string.text_1));
+        addCardItem(new CardItem(R.string.title_4, R.string.text_1));
     }
 
     public void addCardItem(CardItem item) {
         mViews.add(null);
         mData.add(item);
     }
+
+//    public void addPrescriptionCardItem(Prescription prescription) {
+//        mViews.add(null);
+//        mData.add(...)
+//    }
 
     public float getBaseElevation() {
         return mBaseElevation;
@@ -76,6 +98,11 @@ public class CardPagerAdapter extends PagerAdapter implements CardAdapter {
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
         mViews.set(position, null);
+    }
+
+    @Override
+    public void onDataUpdate() {
+
     }
 
     private void bind(final CardItem item, View view) {
