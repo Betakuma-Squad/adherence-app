d5cc07a3c59900684f1de089daa0b1b1f6201317
Russell Blickhan <rwblickhan@gmail.com>
2018-09-16 00:18:27 -0400

    Patch up the list view layout

--- app/src/main/java/com/squad/betakuma/adherence_app/PrescriptionListAdapter.java
+++ app/src/main/java/com/squad/betakuma/adherence_app/PrescriptionListAdapter.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squad.betakuma.adherence_app.swipable_cards.PrescriptionDetailActivity;
@@ -35,21 +36,23 @@ public class PrescriptionListAdapter extends RecyclerView.Adapter<PrescriptionLi
     public static class AdherenceViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public CardView mListItemView;
+        public ImageView mMedicationImageView;
         public TextView mTitleView;
         public TextView mSubtitleView;
+        public TextView mDinView;
         public TextView mQuantityView;
         public TextView mRefillsView;
-        public TextView mDoseView;
         public TextView mInstructionsView;
 
         public AdherenceViewHolder(View v) {
             super(v);
             mListItemView = v.findViewById(R.id.prescription_list_item_view);
+            mMedicationImageView = v.findViewById(R.id.prescription_list_item_medication_img);
             mTitleView = v.findViewById(R.id.prescription_list_item_title_text);
             mSubtitleView = v.findViewById(R.id.prescription_list_item_subtitle_text);
+            mDinView = v.findViewById(R.id.prescription_list_item_din_text);
             mQuantityView = v.findViewById(R.id.prescription_list_item_quantity);
             mRefillsView = v.findViewById(R.id.prescription_list_item_refills);
-            mDoseView = v.findViewById(R.id.prescription_list_item_dose_text);
             mInstructionsView = v.findViewById(R.id.prescription_list_item_instructions_text);
         }
     }
@@ -85,15 +88,21 @@ public class PrescriptionListAdapter extends RecyclerView.Adapter<PrescriptionLi
         // - replace the contents of the view with that element
         final Prescription prescription = mDataset[position];
         final Medication medication = prescription.getMedication();
-        holder.mTitleView.setText(medication.getGenericName());
-        final String subtitle = medication.getBrandName() + ", " + medication.getDIN();
-        holder.mSubtitleView.setText(subtitle);
+        final String title = medication.getGenericName() + " " + medication.getDose();
+        holder.mTitleView.setText(title);
+        holder.mSubtitleView.setText(medication.getBrandName());
+        holder.mDinView.setText(medication.getDIN());
         final String quantity = prescription.getQuantity() + "/" + prescription.getTotalQuantity();
         holder.mQuantityView.setText(quantity);
         final String refills = prescription.getRefills() + "/" + prescription.getTotalRefills();
         holder.mRefillsView.setText(refills);
-        holder.mDoseView.setText(medication.getDose());
         holder.mInstructionsView.setText(prescription.getInstructions());
+        final String imgFilename = "m" + medication.getDIN();
+        holder.mMedicationImageView.setImageResource(
+                mContext.getResources().getIdentifier(mContext.getPackageName() + ":drawable/" + imgFilename,
+                        null,
+                        null));
+
         holder.mListItemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
