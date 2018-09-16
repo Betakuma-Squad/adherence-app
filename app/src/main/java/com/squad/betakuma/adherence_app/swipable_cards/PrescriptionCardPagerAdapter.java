6bd371fac682e60fe705a0329fd951bcdfffc7ca
Russell Blickhan <rwblickhan@gmail.com>
2018-09-16 03:41:57 -0400

    Hardcode our only translation

--- app/src/main/java/com/squad/betakuma/adherence_app/swipable_cards/PrescriptionCardPagerAdapter.java
+++ app/src/main/java/com/squad/betakuma/adherence_app/swipable_cards/PrescriptionCardPagerAdapter.java
@@ -32,6 +32,7 @@ public class PrescriptionCardPagerAdapter extends PagerAdapter implements Prescr
     private float mBaseElevation;
     private DataManager manager;
     private CardTapListener mCardTapListener;
+    private boolean isEnglish = true;
 
     public PrescriptionCardPagerAdapter(Context context, ViewPager viewPager, CardTapListener cardTapListener) {
         mContext = context;
@@ -111,12 +112,36 @@ public class PrescriptionCardPagerAdapter extends PagerAdapter implements Prescr
 //        scrollView.setY(300);
 
         ImageView image = parentView.findViewById(R.id.medication_image);
-        TextView titleText = parentView.findViewById(R.id.medication_title);
+        final TextView titleText = parentView.findViewById(R.id.medication_title);
         TextView subtitleText = parentView.findViewById(R.id.medication_subtitle);
-        TextView collapsedText = parentView.findViewById(R.id.collapsed_content);
-        LinearLayout expandableContent = parentView.findViewById(R.id.card_expanded_content);
-        TextView expandedFullText = parentView.findViewById(R.id.expanded_full_text);
+        final TextView collapsedText = parentView.findViewById(R.id.collapsed_content);
+
+        final LinearLayout expandableContent = parentView.findViewById(R.id.card_expanded_content);
+        final TextView expandedFullText = parentView.findViewById(R.id.expanded_full_text);
         CardView cardView = parentView.findViewById(R.id.cardView);
+        ImageView translateButton = parentView.findViewById(R.id.translate_button);
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (item.getTitle().equals("Ramipril") && isEnglish) {
+                    isEnglish = false;
+                    final String ramiprilChinese =
+                            "雷米普利单独使用或与其他药物一起用于治疗高血压（高血压）。高血压增加了心脏和动脉的工作量。如果持续很长时间，心脏和动脉可能无法正常运作。这会损害大脑，心脏和肾脏的血管，导致中风，心力衰竭或肾衰竭。降低血压可以降低中风和心脏病发作的风险。\n\n"
+                                    + "雷米普利是一种血管紧张素转换酶（ACE）抑制剂。它的作用是阻断体内导致血管收紧的物质。结果，雷米普利使血管松弛。这降低了血压并增加了心脏的血液和氧气供应。\n\n"
+                                    + "雷米普利也用于心脏病发作后的一些患者。心脏病发作后，一些心肌受损并减弱。随着时间的推移，心肌可能继续减弱。这使得心脏泵血更加困难。雷米普利可在心脏病发作后的最初几天内开始，以提高存活率。\n\n"
+                                    + "雷米普利还用于减轻55岁或以上患者发生心脏病或中风的机会，并患有严重的心脏病。";
+                    final String ramiprilTitleChinese = "雷米普利";
+                    collapsedText.setText(ramiprilChinese);
+                    expandedFullText.setText(ramiprilChinese);
+                    titleText.setText(ramiprilTitleChinese);
+                } else if (item.getTitle().equals("Ramipril") && !isEnglish) {
+                    isEnglish = true;
+                    titleText.setText(item.getTitle());
+                    collapsedText.setText(item.getText().replace("\\n", "\n"));
+                    expandedFullText.setText(item.getText().replace("\\n", "\n"));
+                }
+            }
+        });
         item.setCollapsedContent(collapsedText);
         item.setExpandableContent(expandableContent);
         cardView.setOnClickListener(new View.OnClickListener() {
